680281b2acadaff8c075801c302f9ddea2ee7283
Fabio Teixeira da Costa <53267851+ftcosta1976@users.noreply.github.com>
2022-02-25 12:38:23 -0300

    Update server.js
    Testes com git pull

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ app.get('/', (req,res) => {
     res.status(200).send({message: 'Olá mundo estou funcionando!'});
 })
 
+app.get('/Ola', (req,res) => {
+    res.status(200).send({message: 'Oi, eu sou a rota Olá!!'});
+})
+
 app.listen(3001, () => {
     console.log('Api rodando na porta 3001');
 })
